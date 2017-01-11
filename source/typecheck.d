7f7c6c222c2b6d299499656ff783908e9f2373ce
karita <shigekikarita@gmail.com>
2017-01-12 00:27:58 +0900

    refactor type check funcs

--- source/typecheck.d
+++ source/typecheck.d
@@ -1,10 +1,6 @@
-
-import std.range;
+import std.string : format, split;
 import std.meta;
-import std.array;
 import std.traits;
-
-import std.stdio;
 
 
 struct Param(string args) {
@@ -13,57 +9,56 @@ struct Param(string args) {
 }
 
 
-template isArgTypesEqualT(string kargs, Args ...) {
-  enum bool value = is(Param!kargs.Args == AliasSeq!Args);
+bool predicate(alias policy, string kargs, Args ...)(Args args) {
+  return policy!(kargs, Args);
 }
 
-bool isEqualArgTypes(string kargs, Args ...)(Args args) {
-  return isArgTypesEqualT!(kargs, Args).value;
+void staticAssert(alias policy, string kargs, Args ...)(Args args) {
+  static assert(policy!(kargs, Args),
+                "\nleft  args:(%s)\nright args:%s\nthis right args violate the <%s> policy"
+                .format(kargs, Args.stringof, policy.stringof.split("(")[0]));
 }
 
-void assertEqualArgTypes(string kargs, Args ...)(Args args) {
-  static assert(isArgTypesEqualT!(kargs, Args).value,
-                "(%s) is not equal to %s".format(kargs, Args.stringof));
-}
 
+enum bool EqualArgTypes(string kargs, Args ...) = is(Param!kargs.Args == AliasSeq!Args);
 
 unittest {
+  immutable OK = q{int i, float* f, const(char)* c};
+  immutable NG = q{float i, float* f, const(char)* c};
+
   int i = 0;
   float[] f = [1f, 2f];
   const char[] c = ['a'];
-  assert(isEqualArgTypes!q{int i, float* f, const(char)* c}(i, f.ptr, c.ptr));
-  assert(!isEqualArgTypes!q{float i, float* f, const(char)* c}(i, f.ptr, c.ptr));
-  assertEqualArgTypes!q{int i, float* f, const(char)* c}(i, f.ptr, c.ptr);
-  static assert(!__traits(compiles, assertArgTypesEqual!q{float i, float* f, const(char)* c}(i, f.ptr, c.ptr)));
+
+  assert(predicate!(EqualArgTypes, OK)(i, f.ptr, c.ptr));
+  assert(!predicate!(EqualArgTypes, NG)(i, f.ptr, c.ptr));
+
+  staticAssert!(EqualArgTypes, OK)(i, f.ptr, c.ptr);
+  // staticAssert!(EqualArgTypes, NG)(i, f.ptr, c.ptr);
+  static assert(!__traits(compiles, staticAssert!(EqualArgTypes, NG)(i, f.ptr, c.ptr)));
 }
 
 
-template AssignableArgTypesT(string kargs, Args ...) {
+enum bool AssignableArgTypes(string kargs, Args ...) = function() {
   alias KArgs = Param!kargs.Args;
-  enum bool value = function() {
-    bool ok = true;
-    foreach (i, a; Args) {
-      ok &= isAssignable!(KArgs[i], Args[i]);
-    }
-    return ok;
-  }();
-}
-
-bool isAssignableArgTypes(string kargs, Args ...)(Args args) {
-  return AssignableArgTypesT!(kargs, Args).value;
-}
-
-void assertAssignableArgTypes(string kargs, Args ...)(Args args) {
-  static assert(AssignableArgTypesT!(kargs, Args).value,
-                "(%s) is not AssignableArgTypes to %s".format(kargs, Args.stringof));
-}
+  bool ok = true;
+  foreach (i, a; Args) {
+    ok &= isAssignable!(KArgs[i], Args[i]);
+  }
+  return ok;
+}();
 
 unittest {
   int i = 0;
   float[] f = [1f, 2f];
   const char[] c = ['a'];
-  assert(isAssignableArgTypes!q{int i, float* f, const(char)* c}(i, f.ptr, c.ptr));
-  assert(!isAssignableArgTypes!q{int i, float* f, char* c}(i, f.ptr, c.ptr));
-  assertAssignableArgTypes!q{int i, const(float)* f, const(char)* c}(i, f.ptr, c.ptr);
-  static assert(!__traits(compiles, assertAssignableArgs!q{int i, const(float)* f, char* c}(i, f.ptr, c.ptr)));
+  immutable OK = q{int i, float* f, const(char)* c};
+  immutable NG = q{int i, float* f, char* c};
+
+  assert(predicate!(AssignableArgTypes, OK)(i, f.ptr, c.ptr));
+  assert(!predicate!(AssignableArgTypes, NG)(i, f.ptr, c.ptr));
+
+  staticAssert!(AssignableArgTypes, OK)(i, f.ptr, c.ptr);
+  // staticAssert!(AssignableArgTypes, NG)(i, f.ptr, c.ptr);
+  static assert(!__traits(compiles, staticAssert!(AssignableArgTypes, NG)(i, f.ptr, c.ptr)));
 }
